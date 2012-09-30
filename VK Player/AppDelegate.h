2cd45162279dfff4b1fa61945fe1023d810f7f1c
Mihael Isaev <isaev.mihael@gmail.com>
2012-10-01 02:27:24 +0400

    Initial Commit

---
+++ VK Player/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  VK Player
+//
+//  Created by mihael on 01.10.12.
+//  Copyright (c) 2012 Mihael Isaev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ VK Player/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  VK Player
+//
+//  Created by mihael on 01.10.12.
+//  Copyright (c) 2012 Mihael Isaev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
